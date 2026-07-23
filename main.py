a18ce37c959de8aefe8a4d638505f9829c840cfc
AlexisHat <alexis.hatjiefthimiou@gmail.com>
2026-07-23 13:23:43 +0200

    chore: Laufzeit-/Testabhängigkeiten und Paketstruktur
    Alle dependencies aus der Projektskizze hinzugefügt

--- main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from fitnesstracker!")
-
-
-if __name__ == "__main__":
-    main()
---
+++ src/fitlytics/__init__.py
@@ -0,0 +1,3 @@
+"""Fitlytics: analysis of training data from FIT files and recovery CSV data."""
+
+__version__ = "0.1.0"
